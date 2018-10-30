2ba91e6bcfb1bd1a9d370295d15b0dc5fcad29a7
talbert1209 <talbert1209@gmail.com>
2018-10-30 15:11:29 -0500

    Add project files.

---
+++ House/Location.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Specialized;
+
+namespace House
+{
+    public abstract class Location
+    {
+        //Fields & Properties
+        public string Name { get; private set; }
+        public Location[] Exits;
+
+        //Constructor
+        protected Location(string name)
+        {
+            Name = name;
+        }
+
+        //Methods
+        public virtual string Description
+        {
+            get
+            {
+                string description = $"You're standing in the {Name}. You see exits to the following places:";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += $" {Exits[i].Name}";
+                    if (i != Exits.Length - 1)
+                    {
+                        description += ",";
+                    }
+                }
+
+                description += ".";
+                return description;
+            }
+        }
+    }
+}
